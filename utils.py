cb7455a5dc5c98684558b3234831ea8b50f9728b
crops004 <crops004@gmail.com>
2025-07-14 16:13:48 -0600

    Update database connection logic to use local dev DB and production DB on Render

--- utils.py
+++ utils.py
@@ -1,7 +1,15 @@
 import sqlite3
+import os
 
 def get_db_connection():
-    conn = sqlite3.connect('cocktail_app.db')
+    # Default to local dev DB
+    db_file = 'cocktail_dev.db'
+
+    # On Render, use the production DB filename
+    if os.environ.get("RENDER") == "true":
+        db_file = 'cocktail_app.db'
+
+    conn = sqlite3.connect(db_file)
     conn.row_factory = sqlite3.Row
     return conn
 
